afb56279ba8edf4b071d7d6a5fedf836b0496e8b
Ian Cowley <icowley@gmail.com>
2017-08-25 12:51:12 +0100

    Implemented calculation

--- CalcSpike.Core/CalcEngine.cs
+++ CalcSpike.Core/CalcEngine.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalcSpike.Core
+﻿namespace CalcSpike.Core
 {
     public class CalcEngine
     {
         public int Add(int left, int right)
         {
-            throw new NotImplementedException();
+            return left + right;
         }
     }
 }
