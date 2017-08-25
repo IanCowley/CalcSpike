eda708b31ea3601bc8034ea66cd8f6e0b1745344
Ian Cowley <icowley@gmail.com>
2017-08-25 12:51:12 +0100

    Added basic test to prove calculations

---
+++ CalcSpike.Core/CalcEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcSpike.Core
+{
+    public class CalcEngine
+    {
+        public int Add(int left, int right)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CalcSpike.Tests/CalcEngineTests.cs
@@ -0,0 +1,25 @@
+﻿using CalcSpike.Core;
+using Machine.Specifications;
+
+namespace CalcSpike.Tests
+{
+	[Subject(typeof(CalcEngine))]
+    public class CalcEngineTests
+	{
+	    Establish context = () =>
+	    {
+	        _calcEngine = new CalcEngine();
+	    };
+
+	    static CalcEngine _calcEngine;
+
+	    public class when_adding_two_numbers_together
+	    {
+	        Because of = () => _result = _calcEngine.Add(1, 2);
+
+	        It should_have_calculated_result = () => _result.ShouldEqual(3);
+
+            static int _result;
+	    }
+    }
+}
